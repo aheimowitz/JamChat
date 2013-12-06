88a9f983d03d1987c458bd51ca3f5396ceb76957
Mike <mike@Lysander.(none)>
2013-12-06 00:05:59 -0500

    Added neccesary permissions to manifest file to allow wifi direct for app

--- src/com/example/jamchat/MainActivity.java
+++ src/com/example/jamchat/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jamchat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,38 +12,64 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
+	public static final String TAG = "wifidirectdemo";
+	private WifiP2pManager manager;
+	private boolean isWifiP2pEnabled = false;
+	private boolean retryChannel = false;
+	
+	private final IntentFilter intentFilter = new IntentFilter();
+	private Channel channel;
+	//private BroadcastReceiver receiver = null;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		// Setting up onClickListeners for Join Chat button
 		Button loginButton = (Button) findViewById(R.id.useThisButton);
 		loginButton.setOnClickListener(useThisButtonListener);
+		
+		//Adds the neccesary actions to the intentFilter to allow the app to
+		// use Wifi Direct
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        //manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        //channel = manager.initialize(this, getMainLooper(), null);
 	}
 
-	//When the join chat button is pushed make sure the nickname isnt blank
-	//Then call start a new intent and send it the inputted nickname
-	private OnClickListener useThisButtonListener = new OnClickListener() {
-		public void onClick(View v) {
+	//When the join chat button is pushed make sure the nickname isn't blank
+	//Then call start a new intent and send it the inputed nickname
+	private OnClickListener useThisButtonListener = new OnClickListener() 
+	{
+		public void onClick(View v) 
+		{
 			EditText userTypedMessage = (EditText) findViewById(R.id.nameEditText);
 			String nick = userTypedMessage.getText().toString();
-			if (nick.compareTo("") != 0) {
+			if (nick.compareTo("") != 0) 
+			{
 				startNewIntent(nick);
 			}
 		}
 	};
 
 	//Starts a new intent of the messenger, sends it the nickname and starts it
-	public void startNewIntent(String nick) {
+	public void startNewIntent(String nick) 
+	{
 		Intent i = new Intent(this, Messaging.class);
 		i.putExtra("nick", nick);
 		startActivityForResult(i, 9);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
